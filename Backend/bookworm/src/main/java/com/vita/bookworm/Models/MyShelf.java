a6470641a1cddb81487eac00067fc5102326c374
Rohan588~ <rohanrupnawar93@gmail.com>
2025-01-28 19:42:56 +0530

    Pojo Classes updated

--- Backend/bookworm/src/main/java/com/vita/bookworm/Models/MyShelf.java
+++ Backend/bookworm/src/main/java/com/vita/bookworm/Models/MyShelf.java
@@ -5,16 +5,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
 @Data
-public class AttributeMaster {
+public class MyShelf {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "attribute_id")
-    private int attributeId;
+    @Column(name = "shelf_id")
+    private int shelfId;
 
-    @Column(nullable = true,name = "attribute_name")
-    private String attributeName;
+    private int noOfBooks;
+
+
+    @ManyToOne
+    @JoinColumn(name = "shelf_dtl_id", nullable = true)
+    private ShelfDetails shelfDetails;
 }
